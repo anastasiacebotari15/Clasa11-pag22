1b179d60f1133b96bf3b060954d88b4695cf385e
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2021-09-19 22:02:25 +0300

    Add files via upload

---
+++ pag22ex5cl11.py
@@ -0,0 +1,7 @@
+x=[1,2,3,4,5]
+y=[1,2,3,4,5]
+print('Suma primelor trei componente a variabilei x este:', x[0]+x[1]+x[2])
+print('Suma tuturor componentelor variabilei y este:',sum(y))
+print('Produsul tuturor componentelor variabilei x este:', x[0]*x[1]*x[2]*x[3]*x[4])
+print('Valoarea absoluta a componentei a treia a variabilei y este:', abs(y[2]))
+print('Suma primelor componente ale variabilelor x si y este:', x[0]+y[0])
